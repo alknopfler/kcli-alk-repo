958e508edc687fdc995e9e4d1ffac0e1c43de4f3
Alberto Morgante <alknopfler@gmail.com>
2021-02-03 15:47:49 +0100

    update repo

--- registry/deploy.sh
+++ registry/deploy.sh
@@ -2,6 +2,7 @@ REGISTRY_VERSION=2
 REGISTRY_DOMAIN=registry.hub.docker.com/library
 
 REGISTRY_PORT=5000
+setenforce 0
 yum -y install podman
 podman create --name registry --net host --rm -p $REGISTRY_PORT:$REGISTRY_PORT $REGISTRY_DOMAIN/registry:$REGISTRY_VERSION
 podman start registry
