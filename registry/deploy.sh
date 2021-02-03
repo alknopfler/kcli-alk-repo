d223a6eb1107dfe864d9047986fb853c2e0c8c8e
Alberto Morgante <alknopfler@gmail.com>
2021-02-03 16:13:19 +0100

    update repo

--- registry/deploy.sh
+++ registry/deploy.sh
@@ -5,4 +5,4 @@ REGISTRY_PORT=5000
 setenforce 0
 yum -y install podman
 podman create --name registry --net host --rm  --security-opt label=disable -p $REGISTRY_PORT:$REGISTRY_PORT $REGISTRY_DOMAIN/registry:$REGISTRY_VERSION
-podman start -d registry
+podman start registry
